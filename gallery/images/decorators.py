672f305427cd3a20051a0e0d208041d394c32b05
Asher Shores <asher.shores5@gmail.com>
2024-06-12 15:44:09 -0700

    remove django code for new version

--- gallery/images/decorators.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import redirect
-
-def password_protected_view(func):
-    def wrapper(request, *args, **kwargs):
-        if request.session.get('authenticated'):
-            return func(request, *args, **kwargs)
-        return redirect('password_entry')  # Redirect to the password entry view
-
-    return wrapper
--- gallery/images/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import image_list
-from . import views
-
-urlpatterns = [
-    path('password/', views.password_entry, name='password_entry'),
-    path('', image_list, name='image_list'),
-]
-
